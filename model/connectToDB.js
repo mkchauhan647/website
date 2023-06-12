320f2511aeab3ac1bea88fa1ab0ed8710b4c9472
mkchauhan647 <mkchauhan647@gmail.com>
2023-06-12 16:31:02 +0545

    see

--- model/connectToDB.js
+++ model/connectToDB.js
@@ -1,7 +1,11 @@
-import mysql from 'mysql2';
-// const mysql = require('mysql2');
-// const fs = require('fs');
-import fs from 'fs';
+// import mysql from 'mysql2';
+const mysql = require('mysql2');
+const fs = require('fs');
+// import fs from 'fs';
+// import path from 'path';
+const path = require('path');
+const filepath = path.join(__dirname, './DigiCertGlobalRootCA.crt.pem')
+console.log("hellopath:",filepath);
 const connectToDB = () => {
 
     const data = {
@@ -10,7 +14,7 @@ const connectToDB = () => {
         password: "Azure.c0m",
         database: "fundraiser",
         port: 3306,
-        ssl: { ca: fs.readFileSync("/sslca/DigiCertGlobalRootCA.crt.pem") }
+        ssl: { ca: fs.readFileSync(filepath) }
     }
 
 
@@ -31,5 +35,5 @@ const connectToDB = () => {
     }
 }
 
-// connectToDB();
-export default connectToDB;
+connectToDB();
+// export default connectToDB;

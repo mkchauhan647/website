5becd37bc6d16c368a9fa5255cebf219b5db8f4b
mkchauhan647 <mkchauhan647@gmail.com>
2023-06-12 18:06:39 +0545

    check

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -8,6 +8,9 @@ const AboutRoute = async () =>{
         
         <address style={{minHeight:1200}}>
             <h1>Hello this is my address</h1>
+            <h1>Hello this is my address</h1>
+            <h1>Hello this is my address</h1>
+            <h1>Hello this is my address</h1>
         </address>
         
         </>
